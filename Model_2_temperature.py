f25547679e2d6a5718e464e5bbb59de05265761d
MichalGTue <m.j.gajda@student.tue.nl>
2024-09-13 12:44:04 +0200

    Co-authored-by: update Model_2_temperature.py and testfile.py Xikiee <Xikiee@users.noreply.github.com> Co-authored-by

--- Model_2_temperature.py
+++ Model_2_temperature.py
@@ -7,34 +7,45 @@ import scipy.integrate
 # Assume constant density
 
 # Flow parameters
-fv_w = 161.10420227050781 # Water Flow Rate (ml/min)
-fv_a = 11.524953842163086 # Anhydride Flow Rate (ml/min)
-v_cstr = 100 # Volume of CSTR (ml)
+fv_w = 161.10420227050781  # Water Flow Rate (ml/min)
+fv_a = 11.524953842163086  # Anhydride Flow Rate (ml/min)
 
-#Convert flows to units (ml/s)
-fv_w_s = fv_w/60
-fv_a_s = fv_a/60
+v_cstr = 200  # Volume of CSTR (ml)
+# Convert flow rates to consistent units (ml/min to ml/s)
+fv_w_dm3_s = fv_w  / 60  # Water flow rate in ml/s
+fv_a_dm3_s = fv_a  / 60  # Anhydride flow rate in ml/s
 
-#Make an array with the flows
-flow_array = [fv_w_s,fv_a_s]
+#Inlet Temperature 
+T0 = 25 # (celsius)
+
+#Chemical constants
+mm_water = 18.01528 # (g/mol)
+rho_water = 0.999842 # (g/ml)
+cw_pure = rho_water/mm_water
+
+mm_AAH = 102.089 # (g/mol)
+rho_AAH = 1.082 # (g/ml)
+caah_pure = rho_AAH/mm_AAH
+
+flow_array = [fv_w_dm3_s, fv_a_dm3_s]
+
 
 #Thermodynamic constants
 params = {
-    "C0_water": 13,
+    "C_in_water": (flow_array[0]*cw_pure)/(flow_array[0]+flow_array[1]),
+    "C_in_AAH": (flow_array[1]*caah_pure)/(flow_array[0]+flow_array[1]),
+    "Inlet temperature": T0+273.15,
     "flow": flow_array,
     "V": v_cstr,  # Volume in ml
-    "k0": 1.8725e3,          # Reaction rate constant (ml/mol/s)
+    "k0": np.exp(16.25)*1000,          # Reaction rate constant (ml/mol/s)
 
     # Thermodynamic constants (taken from Asprey et al., 1996)
     "Ea": 45622.34,             # Activation energy (J/mol)
     "R": 8.314,              # Gas constant (J/mol/K)
-    "H": 56.6e3,              # Enthalpy change (J/mol)
+    "H": -56.6e3,              # Enthalpy change (J/mol)
     "rho": 1,            # Density (g/ml)
     "cp": 4.186             # Heat capacity (J/g/K)
 }
-
-
-
 
 
 def der_func(t,C, parameters):
@@ -42,6 +53,8 @@ def der_func(t,C, parameters):
     dcdt = np.zeros(4)
 
     # Getting parameters
+    C_in_w = parameters['C_in_water']
+    C_in_AAH = parameters['C_in_AAH']
     flow = parameters['flow']
     V = parameters['V']
     k0 = parameters['k0']
@@ -50,17 +63,17 @@ def der_func(t,C, parameters):
     H = parameters['H']
     rho = parameters['rho']
     cp = parameters['cp']
+    inlet_temp = parameters["Inlet temperature"]
     
-    
-    reaction_rate = C[0]*C[1] * k0  * np.exp(-Ea/(R*C[3]))
-    
+    reaction_rate = C[0]*C[1] * k0 * np.exp(-Ea/(R*C[3]))
+
+
     total_flow = flow[0]+flow[1]
- 
     #Differential equations
-    dcdt[0] =  (flow[0]/V)*(2 - C[0])    - reaction_rate # reaction_rate # Water Concentration derv
-    dcdt[1] =  (flow[1]/V)*(1 - C[1])    - reaction_rate # reaction_rate # Anhydride Concentration derv
+    dcdt[0] =  (flow[0]/V)*(C_in_w - C[0])    - reaction_rate # reaction_rate # Water Concentration derv
+    dcdt[1] =  (flow[1]/V)*(C_in_AAH - C[1])  - reaction_rate  # Anhydride Concentration derv
     dcdt[2] =  (total_flow/V)*(0 - C[2]) + 2*reaction_rate # 2*reaction_rate # Acetic acid 
-    dcdt[3] =  (total_flow/V) * (298-C[3]) - H/(rho*cp) * reaction_rate # Temperature part
+    dcdt[3] =  (total_flow/V) * (inlet_temp-C[3]) - H/(rho*cp) * reaction_rate # Temperature part
     return dcdt
 
 
@@ -68,18 +81,24 @@ def der_func(t,C, parameters):
 tspan = [0,600] # Time in seconds
 
 #For xini, c_water_0, c_AAH_0, C_AA_0, T0(in k)
-xini = [22,0,0,298] # Initial Conditions 
+xini = [cw_pure,0,0,T0+273.15] # Initial Conditions 
 
 sol = scipy.integrate.solve_ivp(der_func, tspan, xini, args=(params,))
 
-plt.plot(sol.t, sol.y[0], label='Conc. Water')
+#plt.plot(sol.t, sol.y[0], label='Conc. Water')
 plt.plot(sol.t, sol.y[1], label='Conc. AAH')
 plt.plot(sol.t, sol.y[2], label='Conc. AA')
 plt.xlabel('time')
-plt.ylabel('Concentration(mol/L)')
+plt.ylabel('Concentration(mol/mL)')
 plt.legend()
 plt.title('Concentration of various components in a CSTR')
 plt.show()
 
 plt.plot(sol.t, sol.y[3])
 plt.show()
+
+
+
+# check the conversion and graph data due to suspicion of low aah conc
+
+mol_aah = tspan[1]*caah_pure*flow_array[1]
--- testfile.py
+++ testfile.py
@@ -7,11 +7,26 @@ fv_w = 161.10420227050781  # Water Flow Rate (ml/min)
 fv_a = 11.524953842163086  # Anhydride Flow Rate (ml/min)
 v_cstr = 500  # Volume of CSTR (ml)
 
-# Convert flow rates to consistent units (ml/min to dm³/s)
-fv_w_dm3_s = fv_w  / 60  # Water flow rate in dm³/s
-fv_a_dm3_s = fv_a  / 60  # Anhydride flow rate in dm³/s
+#Inlet Temperature 
+T0 = 25 # (celsius)
+
+# Convert flow rates to consistent units (ml/min to ml/s)
+fv_w_dm3_s = fv_w  / 60  # Water flow rate in ml/s
+fv_a_dm3_s = fv_a  / 60  # Anhydride flow rate in ml/s
+
+#Chemical constants
+mm_water = 18.01528 # (g/mol)
+rho_water = 0.999842 # (g/ml)
+cw_pure = rho_water/mm_water
+
+mm_AAH = 102.089 # (g/mol)
+rho_AAH = 1.082 # (g/ml)
+caah_pure = rho_AAH/mm_AAH
 
 flow_array = [fv_w_dm3_s, fv_a_dm3_s]
+
+cw0 = (flow_array[0]*cw_pure)/(flow_array[0]+flow_array[1])
+caah0 = (flow_array[0]*caah_pure)/(flow_array[0]+flow_array[1])
 
 # Thermodynamic constants
 t_cstr = 25  # CSTR Temperature (°C)
@@ -37,8 +52,10 @@ def der_func(t, C, flow=flow_array, V=1, k0=k_1):
 # Time span for integration (minutes)
 tspan = [0, 60]  # Time in minutes
 
+
 # Initial conditions: concentrations of Water, Anhydride, Acetic Acid, Temperature
 xini = [2.0, 1.0, 0.0, 25]  # Initial concentrations (mol/L) and initial temperature (°C)
+
 
 # Solve ODE
 sol = scipy.integrate.solve_ivp(der_func, tspan, xini)
@@ -48,7 +65,7 @@ plt.plot(sol.t, sol.y[0], label='Water (Ca)')
 plt.plot(sol.t, sol.y[1], label='Anhydride (Cb)')
 plt.plot(sol.t, sol.y[2], label='Acetic Acid (Cc)')
 plt.xlabel('Time (min)')
-plt.ylabel('Concentration (mol/L)')
+plt.ylabel('Concentration (mol/mL)')
 plt.legend()
 plt.title('Concentration of Various Components in a CSTR')
 plt.show()
