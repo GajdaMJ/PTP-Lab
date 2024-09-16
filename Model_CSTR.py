40b5d3df360621683b01a653ceb2e062030b448c
MichalGTue <m.j.gajda@student.tue.nl>
2024-09-16 11:41:34 +0200

    update 1 file and copy 1 file

--- Model_CSTR.py
+++ Model_CSTR.py
@@ -65,20 +65,20 @@ def der_func(t,C, parameters):
     cp = parameters['cp']
     inlet_temp = parameters["Inlet temperature"]
     
-    reaction_rate = C[0]*C[1] * k0 * np.exp(-Ea/(R*C[3])) *1e-3
+    reaction_rate = C[0]*C[1] * k0 * np.exp(-Ea/(R*C[3])) 
 
 
     total_flow = flow[0]+flow[1]
     #Differential equations
-    dcdt[0] =  (flow[0]/V)*(C_in_w - C[0])    - reaction_rate # reaction_rate # Water Concentration derv
-    dcdt[1] =  (flow[1]/V)*(C_in_AAH - C[1])  - reaction_rate  # Anhydride Concentration derv
+    dcdt[0] =  (flow[0]/V)*(C_in_w) - (total_flow/V)*C[0]    - reaction_rate # reaction_rate # Water Concentration derv
+    dcdt[1] =  (flow[1]/V)*(C_in_AAH) - (total_flow/V)*C[1]  - reaction_rate  # Anhydride Concentration derv
     dcdt[2] =  (total_flow/V)*(0 - C[2]) + 2*reaction_rate # 2*reaction_rate # Acetic acid 
     dcdt[3] =  (total_flow/V) * (inlet_temp-C[3]) - H/(rho*cp) * reaction_rate # Temperature part
     return dcdt
 
 
 
-tspan = [0,6000] # Time in seconds
+tspan = [0,600] # Time in seconds
 
 #For xini, c_water_0, c_AAH_0, C_AA_0, T0(in k)
 xini = [cw_pure,0,0,T0+273.15] # Initial Conditions 
