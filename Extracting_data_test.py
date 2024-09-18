e456acf9bc242d05f1efb39995eeab9c4626bcc6
X1kiee <oliverl.lohr@gmail.com>
2024-09-18 14:28:59 +0200

    sync

--- Extracting_data_test.py
+++ Extracting_data_test.py
@@ -1,30 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
-my_data = np.genfromtxt('Data/test_11.09_cstr_AAHFlow.csv', delimiter=';', dtype=None, names=True, encoding=None)
+my_data = np.genfromtxt('Data/CSTR/Runs 16.09/CSTR 27c.csv', delimiter=';', dtype=None, names=True, encoding=None)
 
 print(my_data)
 
 
 
-import numpy as np
-import csv
-import pandas as pd
-import matplotlib.pyplot as plt
+# import numpy as np
+# import csv
+# import pandas as pd
+# import matplotlib.pyplot as plt
 
-df = pd.read_csv("CSTR/CSTR_27c.csv", sep = ";")
-tagname = df['TagName'].values
-time = df['DateTime'].values
-value = df['vValue'].values
+# df = pd.read_csv("Data/CSTR/Runs 16.09/CSTR 27c.csv", sep = ";")
+# tagname = df['TagName'].values
+# time = df['DateTime'].values
+# value = df['vValue'].values
 
 
-#making the data frame with time as index
-data_1 = {'tagname' :tagname, 'time': time,'value':value}
-data_frame = pd.DataFrame(data=data_1, index=tagname)
+# #making the data frame with time as index
+# data_1 = {'tagname' :tagname, 'time': time,'value':value}
+# data_frame = pd.DataFrame(data=data_1, index=tagname)
 
-#the data frame with time and values in columns
-# data_1 = {'aflow':aflow}
-# data_frame = pd.DataFrame(data=data_1, index=time)
+# #the data frame with time and values in columns
+# # data_1 = {'aflow':aflow}
+# # data_frame = pd.DataFrame(data=data_1, index=time)
 
-print(data_frame)
+# print(data_frame)
 
