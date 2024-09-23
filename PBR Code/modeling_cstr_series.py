a33e2aa07b595455b08ff2c64c9e7b5e35bcc9fc
X1kiee <oliverl.lohr@gmail.com>
2024-09-23 14:27:45 +0200

    modeling cstr in series

--- PBR Code/modeling_cstr_series.py
+++ PBR Code/modeling_cstr_series.py
@@ -1,1 +1,21 @@
 #modeling the pfr as a cstr system in series with 7 cstr
+
+
+import numpy as np
+import matplotlib.pyplot as plt 
+
+
+def function(t,):
+    dxdt = np.zeros(7)
+    
+    dxdt[0] = 
+    dxdt[1] =
+    dxdt[2] =
+    dxdt[3] =
+    dxdt[4] =
+    dxdt[5] =
+    dxdt[6] =
+
+    return dxdt 
+
+
