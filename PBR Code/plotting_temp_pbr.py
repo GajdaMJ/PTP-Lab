546feb38d83516c21c71145ecaf3330faf52d34f
X1kiee <oliverl.lohr@gmail.com>
2024-10-01 18:57:31 +0200

    plotting temp data from 30C

--- PBR Code/plotting_temp_pbr.py
+++ PBR Code/plotting_temp_pbr.py
@@ -60,35 +60,35 @@ plt.figure(figsize=(12, 8))
 
 # Plot for 208 data
 # plt.plot(elapsed_time_208, temp_208, 'o', label='T208 Raw Data', color='#1f77b4')  # Blue
-plt.plot(elapsed_time_interp, temp_208_smooth, '-', color='#ff7f0e', label='T208 Smoothed Curve')  # Orange
+plt.plot(elapsed_time_interp, temp_208_smooth - temp_208[0], '-', color='#ff7f0e', label='T208 Smoothed Curve')  # Orange
 
 # Plot for 207 data
 # plt.plot(elapsed_time_207, temp_207, 'o', label='T207 Raw Data', color='#2ca02c')  # Green
-plt.plot(elapsed_time_interp, temp_207_smooth, '-', color='#d62728', label='T207 Smoothed Curve')  # Red
+plt.plot(elapsed_time_interp, temp_207_smooth - temp_207[0], '-', color='#d62728', label='T207 Smoothed Curve')  # Red
 
 # Plot for 206 data
 # plt.plot(elapsed_time_206, temp_206, 'o', label='T206 Raw Data', color='#9467bd')  # Purple
-plt.plot(elapsed_time_interp, temp_206_smooth, '-', color='#8c564b', label='T206 Smoothed Curve')  # Brown
+plt.plot(elapsed_time_interp, temp_206_smooth -temp_206[0], '-', color='#8c564b', label='T206 Smoothed Curve')  # Brown
 
 # Plot for 205 data
 # plt.plot(elapsed_time_205, temp_205, 'o', label='T205 Raw Data', color='#e377c2')  # Pink
-plt.plot(elapsed_time_interp, temp_205_smooth, '-', color='#7f7f7f', label='T205 Smoothed Curve')  # Gray
+plt.plot(elapsed_time_interp, temp_205_smooth-temp_205[0], '-', color='#7f7f7f', label='T205 Smoothed Curve')  # Gray
 
 # Plot for 204 data
 # plt.plot(elapsed_time_204, temp_204, 'o', label='T204 Raw Data', color='#bcbd22')  # Yellow-green
-plt.plot(elapsed_time_interp, temp_204_smooth, '-', color='#17becf', label='T204 Smoothed Curve')  # Cyan
+plt.plot(elapsed_time_interp, temp_204_smooth-temp_204[0], '-', color='#17becf', label='T204 Smoothed Curve')  # Cyan
 
 # Plot for 203 data
 # plt.plot(elapsed_time_203, temp_203, 'o', label='T203 Raw Data', color='#ff9896')  # Light Red
-plt.plot(elapsed_time_interp, temp_203_smooth, '-', color='#c49c94', label='T203 Smoothed Curve')  # Light Brown
+plt.plot(elapsed_time_interp, temp_203_smooth-temp_203[0], '-', color='#c49c94', label='T203 Smoothed Curve')  # Light Brown
 
 # Plot for 202 data
 # plt.plot(elapsed_time_202, temp_202, 'o', label='T202 Raw Data', color='#aec7e8')  # Light Blue
-plt.plot(elapsed_time_interp, temp_202_smooth, '-', color='#ffbb78', label='T202 Smoothed Curve')  # Light Orange
+plt.plot(elapsed_time_interp, temp_202_smooth-temp_202[0], '-', color='#ffbb78', label='T202 Smoothed Curve')  # Light Orange
 
 # Plot for 201 data
 # plt.plot(elapsed_time_201, temp_201, 'o', label='T201 Raw Data', color='#98df8a')  # Light Green
-plt.plot(elapsed_time_interp, temp_201_smooth, '-', color='#f7b6d2', label='T201 Smoothed Curve')  # Light Pink
+plt.plot(elapsed_time_interp, temp_201_smooth-temp_201[0], '-', color='#f7b6d2', label='T201 Smoothed Curve')  # Light Pink
 
 # Labeling the axes
 plt.xlabel('Elapsed Time (min)')
